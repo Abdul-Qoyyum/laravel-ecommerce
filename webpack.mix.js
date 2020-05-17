1a57c57bcb7769a3af53af4d09508b8fb44f4166
Abdul-Qoyyum <nurudeen96qoyyum@gmail.com>
2020-05-17 07:26:13 -0400

    Make the admin section more modular

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js(['resources/assets/js/core/jquery.min.js',
-                'resources/assets/js/core/popper.min.js',
-                'resources/assets/js/core/bootstrap.min.js',
-                'resources/assets/js/plugins/perfect-scrollbar.jquery.min.js',
-                'resources/assets/js/plugins/chartjs.min.js',
-                'resources/assets/js/plugins/bootstrap-notify.js',
-                'resources/assets/js/now-ui-dashboard.min.js'],'public/js/all.js')
-    .styles(['resources/assets/css/bootstrap.min.css',
-             'resources/assets/css/now-ui-dashboard.css'],'public/css/all.css')
     .sass('resources/sass/app.scss', 'public/css');
